317147a917dedd4b61e16eb9aa3f5581debabb86
Luiz Felipe Machado <luizfelmach@gmail.com>
2026-05-12 10:16:48 -0300

    chore: remove unused constants

--- src/main/java/com/frauddetection/config/KafkaConfig.java
+++ src/main/java/com/frauddetection/config/KafkaConfig.java
@@ -9,16 +9,11 @@ import org.apache.kafka.common.serialization.StringSerializer;
 
 public class KafkaConfig {
 
-    public static final String BOOTSTRAP_SERVERS = "localhost:19092,localhost:29092,localhost:39092";
+    public static final String BOOTSTRAP_SERVERS =
+        "localhost:19092,localhost:29092,localhost:39092";
 
     public static final String TOPIC_TRANSACTIONS_RAW = "transactions.raw";
     public static final String TOPIC_AUTH_EVENTS = "auth.events";
-    public static final String TOPIC_CUSTOMER_PROFILE = "customer.profile";
-    public static final String TOPIC_FRAUD_ALERTS = "fraud.alerts";
-    public static final String TOPIC_RISK_SCORES = "risk.scores";
-    public static final String TOPIC_TRANSACTIONS_BLOCKED = "transactions.blocked";
-    public static final String TOPIC_TRANSACTIONS_APPROVED = "transactions.approved";
-    public static final String TOPIC_TRANSACTIONS_REVIEW = "transactions.review";
 
     public static Properties producerProps() {
         Properties props = new Properties();
@@ -43,10 +38,7 @@ public class KafkaConfig {
             ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG,
             StringDeserializer.class.getName()
         );
-        props.put(
-            ConsumerConfig.AUTO_OFFSET_RESET_CONFIG,
-            "earliest"
-        );
+        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
         return props;
     }
 }
